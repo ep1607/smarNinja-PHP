75475fc803f970e01fe6db37ab7b53918ca7f791
Eva Počivavšek Janhar <eva.pocivavsek.janhar@gmail.com>
2016-02-17 22:03:09 +0000

    vnosno polje sedaj sprejema samo pozitivna stevila

--- fizzbuzz/dn_5_fizzbuzz.php
+++ fizzbuzz/dn_5_fizzbuzz.php
@@ -6,29 +6,35 @@
 
     $meja = $_POST['meja'];
 
-    for($i=1; $i<=$meja; $i++) 
+    if (is_numeric($meja) && $meja>=0 )
     {
-              
-        if($i % 3 == 0 && $i % 5 ==0)
+        for($i=1; $i<=$meja; $i++) 
         {
-            echo 'fizzbuzz';
+            if($i % 3 == 0 && $i % 5 ==0)
+            {
+                echo 'fizzbuzz';
+            }
+            elseif($i % 3 == 0) 
+            {
+                echo 'fizz';
+            }
+            elseif($i % 5 == 0) 
+            {
+                echo 'buzz';
+            }
+            else 
+            {
+                echo $i;
+            }
+
+            echo ', ';
         }
-        elseif($i % 3 == 0) 
-        {
-            echo 'fizz';
-        }
-        elseif($i % 5 == 0) 
-        {
-            echo 'buzz';
-        }
-        else 
-        {
-            echo $i;
-        }
-        
-        echo ', ';
+
+        echo '. . .';
+    } 
+    else
+    {
+        echo "Vnesi pozitivno stevilo.";
     }
-
-    echo '. . .';
-
+    
 ?>
